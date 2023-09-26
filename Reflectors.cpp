bba902c62b299156f83310826ad3e9c057cdc2fb
NadaEAe <nada.elarabi2@gmail.com>
2023-09-25 21:42:50 -0400

    Just began implementation of Reflectors class

---
+++ Reflectors.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Nada El Arabi on 2023-09-25.
+//
+
+#include "Reflectors.h"
+
+Reflectors::Reflectors(char reflectorLetter) {
+    if(reflectorLetter == 64){
+        wiring = "";
+    }
+}
---
+++ Reflectors.h
@@ -0,0 +1,16 @@
+//
+// Created by Nada El Arabi on 2023-09-25.
+//
+#include <iostream>
+
+class Reflectors{
+
+    private:
+    char Reflector;
+    std::string wiring;
+
+    public:
+    Reflectors();
+    Reflectors(char reflectorLetter);
+
+};
